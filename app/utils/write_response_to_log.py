1d41575a9decd79c7453ea43522c4e7440ece3f1
g-battaglia <battaglia.giacomo@icloud.com>
2023-10-05 22:49:14 +0200

    Updates

--- app/utils/write_response_to_log.py
+++ app/utils/write_response_to_log.py
@@ -8,7 +8,7 @@ from fastapi import Request
 
 
 def get_write_response_to_log(logger: Logger):
-    def write_response_to_log(level, request: Request, message: str):
+    def write_response_to_log(level, request: Request, message: str | Exception):
         logger.log(level, f"{str(request.url)}: {str(message)}")
 
     return write_response_to_log
