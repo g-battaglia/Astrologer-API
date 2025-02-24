8d9e4d80e21e909bca6719cce6fe5513ba68cc16
g-battaglia <battaglia.giacomo@icloud.com>
2025-02-24 20:16:25 +0100

    KR Updated

--- app/types/response_models.py
+++ app/types/response_models.py
@@ -1,15 +1,19 @@
 from pydantic import BaseModel, Field
 
 from kerykeion.kr_types import LunarPhaseModel, AstrologicalSubjectModel
+from kerykeion.kr_types import Quality, Element, Sign, Houses, Planet, AxialCusps, AspectName, SignsEmoji, SignNumbers, PointType, ZodiacType
 from typing import Optional
 
 
 class AspectModel(BaseModel):
-    p1_name: str = Field(description="The name of the first planet.")
+    """
+    The model for the aspects, similar to the one in the Kerykeion library.
+    """
+    p1_name: Planet | AxialCusps = Field(description="The name of the first planet.")
     p1_abs_pos: float = Field(description="The absolute position of the first planet.")
-    p2_name: str = Field(description="The name of the second planet.")
+    p2_name: Planet | AxialCusps = Field(description="The name of the second planet.")
     p2_abs_pos: float = Field(description="The absolute position of the second planet.")
-    aspect: str = Field(description="The aspect between the two planets.")
+    aspect: AspectName = Field(description="The aspect between the two planets.")
     orbit: float = Field(description="The orbit between the two planets.")
     aspect_degrees: float = Field(description="The degrees of the aspect.")
     diff: float = Field(description="The difference between the two planets.")
@@ -18,21 +22,28 @@ class AspectModel(BaseModel):
 
 
 class PlanetModel(BaseModel):
-    name: str = Field(description="The name of the planet.")
-    quality: str = Field(description="The quality of the planet.")
-    element: str = Field(description="The element of the planet.")
-    sign: str = Field(description="The sign in which the planet is located.")
-    sign_num: int = Field(description="The number of the sign in which the planet is located.")
+    """
+    The model for the planets, similar to the one in the Kerykeion library.
+    """
+
+    name: Planet | AxialCusps = Field(description="The name of the planet.")
+    quality: Quality = Field(description="The quality of the planet.")
+    element: Element = Field(description="The element of the planet.")
+    sign: Sign = Field(description="The sign in which the planet is located.")
+    sign_num: SignNumbers = Field(description="The number of the sign in which the planet is located.")
     position: float = Field(description="The position of the planet inside the sign.")
     abs_pos: float = Field(description="The absolute position of the planet in the 360 degrees circle of the zodiac.")
-    emoji: str = Field(description="The emoji of the planet.")
-    point_type: str = Field(description="The type of the point.")
-    house: Optional[str] = Field(description="The house in which the planet is located.")
+    emoji: SignsEmoji = Field(description="The emoji of the sign in which the planet is located.")
+    point_type: PointType = Field(description="The type of the point.")
+    house: Optional[Houses] = Field(description="The house in which the planet is located.")
     retrograde: Optional[bool] = Field(default=None, description="The retrograde status of the planet.")
 
 
 class BirthDataModel(BaseModel):
-    # Data
+    """
+    The model for the birth data.
+    """
+
     name: str = Field(description="The name of the subject.")
     year: int = Field(description="Year of birth.")
     month: int = Field(description="Month of birth.")
@@ -44,7 +55,7 @@ class BirthDataModel(BaseModel):
     lng: float = Field(description="Longitude of birth.")
     lat: float = Field(description="Latitude of birth.")
     tz_str: str = Field(description="Timezone of birth.")
-    zodiac_type: str = Field(description="The type of zodiac used.")
+    zodiac_type: ZodiacType = Field(description="The type of zodiac used.")
     local_time: str = Field(description="The local time of birth.")
     utc_time: str = Field(description="The UTC time of birth.")
     julian_day: float = Field(description="The Julian day of birth.")
@@ -62,6 +73,12 @@ class BirthDataModel(BaseModel):
     pluto: PlanetModel = Field(description="The data of Pluto.")
     chiron: PlanetModel = Field(description="The data of Chiron.")
 
+    # Axial Cusps
+    asc: PlanetModel = Field(description="The data of the ascendant.")
+    dsc: PlanetModel = Field(description="The data of the descendant.")
+    mc: PlanetModel = Field(description="The data of the midheaven.")
+    ic: PlanetModel = Field(description="The data of the imum coeli.")
+    
     # Houses
     first_house: PlanetModel = Field(description="The data of the first house.")
     second_house: PlanetModel = Field(description="The data of the second house.")
@@ -81,15 +98,21 @@ class BirthDataModel(BaseModel):
     true_node: PlanetModel = Field(description="The data of the true node.")
 
     # Lunar Phase
-    lunar_phase: Optional[LunarPhaseModel]
+    lunar_phase: Optional[LunarPhaseModel] = Field(description="The lunar phase of the subject.")
 
 
 class BirthDataResponseModel(BaseModel):
+    """
+    The response model for the Birth Data endpoint.
+    """
     status: str = Field(description="The status of the response.")
     data: BirthDataModel = Field(description="The data of the subject.")
 
 
 class BirthChartResponseModel(BaseModel):
+    """
+    The response model for the Birth Chart endpoint.
+    """
     status: str = Field(description="The status of the response.")
     data: BirthDataModel = Field(description="The data of the subject.")
     chart: str = Field(description="The SVG chart of the birth chart.")
@@ -97,11 +120,17 @@ class BirthChartResponseModel(BaseModel):
 
 
 class DoubleDataModel(BaseModel):
+    """
+    The model for the data of two subjects.
+    """
     first_subject: AstrologicalSubjectModel = Field(description="The data of the first subject.")
     second_subject: AstrologicalSubjectModel = Field(description="The data of the second subject.")
 
 
 class SynastryChartResponseModel(BaseModel):
+    """
+    The response model for the Synastry.
+    """
     status: str = Field(description="The status of the response.")
     data: DoubleDataModel = Field(description="The data of the two subjects.")
     chart: str = Field(description="The SVG chart of the synastry.")
@@ -109,6 +138,10 @@ class SynastryChartResponseModel(BaseModel):
 
 
 class RelationshipScoreResponseModel(BaseModel):
+    """
+    The response model for the Relationship Score endpoint.
+    """
+
     status: str = Field(description="The status of the response.")
     data: DoubleDataModel = Field(description="The data of the two subjects.")
     score: float = Field(description="The relationship score between the two subjects.")
